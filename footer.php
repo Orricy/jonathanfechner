a88e5579a4b42ca81b6b62a743b46f1da0b83573
Oricy <d.duvernois@student.isartdigital.com>
2015-11-17 15:32:35 +0100

    add footer content
    large edit of scss

--- footer.php
+++ footer.php
@@ -1,10 +1,54 @@
 <div class="container-fluid footerContent">
-	
+	<div class="container">
+		<div class="col-md-4">
+			<h3 class="text-center">Contact</h3>
+			<h4 class="text-center"><strong>Jonathan Fechner</strong></h4>
+			<div class="col-md-8 col-md-offset-2">
+				<a class="btn btn-default btn-block" href="#" role="button">Contacter</a>
+			</div>
+			<!--<h4><li class="glyphicon glyphicon-phone"></li> 06.13.24.48.61</h4>
+			<h4><i class="glyphicon glyphicon-envelope"></i> contact@jonathanfechner.fr</h4>-->
+		</div>
+		<div class="col-md-4">
+			<h3 class="text-center">Liens utiles</h3>
+			<p><a href="https://fr.wikipedia.org/wiki/Optimisation_pour_les_moteurs_de_recherche">Qu'est ce que c'est ?</a></p>
+			<p><a href="http://www.commentcamarche.net/contents/1267-referencement-naturel-seo-guide-pratique-complet">Comment ça marche</a></p>
+			<p><a href="https://checklists.opquast.com/seo/">Liste des bonnes pratiques</a></p>
+		</div>
+		<div class="col-md-4">
+			<h3 class="text-center">Social</h3>
+			<div class="col-md-2">
+				<a href="#">
+				  	<span class="fa fa-facebook-square socialIcon"></span>
+				</a>
+			</div>
+			<div class="col-md-2">
+				<a href="#">
+				  	<span class="fa fa-linkedin-square socialIcon"></span>
+				</a>
+			</div>
+			<div class="col-md-2">
+				<a href="#">
+				  	<span class="fa fa-twitter-square socialIcon"></span>
+				</a>
+			</div>
+			<div class="col-md-2">
+				<a href="#">
+				  	<span class="fa fa-youtube-square socialIcon"></span>
+				</a>
+			</div>
+			<div class="col-md-2">
+				<a href="#">
+				  	<span class="fa fa-google-plus-square socialIcon"></span>
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
 <div class="container-fluid">
 	<div class="text-center">
 		<small>Jonathan Fechner, consultant et formateur T.I.C - Mentions Légales - Plan du site - Tous droits réservés ©copyright 2015</small><br>
-		<small>Par Damien Duvernois, Shana Enkaoua, Rami El Khodry, Gauthier Flichy, Niki Gharabaghi,Paul Dabin</small>
+		<small>Par Damien Duvernois, Shana Enkaoua, Rami El Khodry, Gauthier Flichy, Niki Gharabaghi, Paul Dabin</small>
 	</div>
 </div>
 <script type="text/javascript" src="lib/jquery-2.1.1.min.js"></script>
--- index.php
+++ index.php
@@ -90,14 +90,14 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12 col-md-offset-0">
-					<div class="col-md-6">
+					<article class="col-md-6">
 						<h3>iiiiiiiii</h3>
 						<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?</p>
-					</div>
-					<div class="col-md-6">
+					</article>
+					<article class="col-md-6">
 						<h3>ccccccccc</h3>
 						<p>Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?</p>
-					</div>
+					</article>
 				</div>
 			</div>
 		</div>
