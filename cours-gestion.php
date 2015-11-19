dc05b73d6de6640f9129411fb1d198b45409733f
Oricy <d.duvernois@student.isartdigital.com>
2015-11-19 18:49:14 +0100

    Update meta-data

--- cours-gestion.php
+++ cours-gestion.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr-FR">
 <head>
-	<title>Jonathan Fechner - accueil</title>
+	<title>Jonathan Fechner - Gestion</title>
 	<!-- Dublin Core -->
 	<meta charset="utf-8">
 	<meta name="DC.title" content="Jonathan Fechner">
 	<meta name="DC.creator" content="Jonathan Fechner">
-	<meta name="DC.subject" content="mots clefs" />
-	<meta name="DC.description" content="description du site" />
-	<meta name="DC.publisher" content="Auteur">
-	<meta name="DC.contributor" content="Contributeur">
-	<meta name="DC.date" content="date de création">
-	<meta name="DC.type" content="type de site">
+	<meta name="DC.subject" content="relation, client, cahier des charges, projet, gestion de projet, gérer, équipe, site internet">
+	<meta name="DC.description" content="Jonathan Fechner expert en gestion de projet. Vous donne quelques conseils pour bien tenir le rôle de chef de projet">
+	<meta name="DC.publisher" content="Damien Duvernois,Gauthier Flichy, Paul Dabin">
+	<meta name="DC.contributor" content="Shana Enkaoua, Rami El Khodry, Niki Gharabaghi">
+	<meta name="DC.date" content="19/11/2015">
+	<meta name="DC.type" content="Cours">
 	<meta name="DC.format" content="website">
-	<meta name="DC.identifier" content="url du site">
+	<meta name="DC.identifier" content="http://www.jonathanfechner.fr/">
 	<meta name="DC.language" content="fr-FR">
 	<meta name="DC.coverage" content="World">
-	<meta name="DC.rights" content="&copy; nom de l'entreprise ou de l'auteur">
+	<meta name="DC.rights" content="&copy; Jonathan Fechner">
 	<!-- END Dublin Core -->
 	<!-- Referencement -->
-	<meta name="description" content="description du site">
-	<meta name="keywords" content="mots clefs">
-	<meta name="author" content="auteur">
-	<meta name="robots" content="index">
-	<meta name="Indentifier-URL" content="url du site">
+	<meta name="description" content="Jonathan Fechner expert en gestion de projet. Vous donne quelques conseils pour bien tenir le rôle de chef de projet">
+	<meta name="keywords" content="relation, client, cahier des charges, projet, gestion de projet, gérer, équipe, site internet">
+	<meta name="author" content="Jonathan Fechner">
+	<meta name="robots" content="cours - gestion">
+	<meta name="Indentifier-URL" content="http://www.jonathanfechner.fr/">
 	<!-- END Référencement -->
 	<!-- Open Graph -->
-	<meta property="og:title" content="titre du site">
+	<meta property="og:title" content="Jonathan Fechner">
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="url du site">
-	<meta property="og:image" content="url de l'image à afficher">
-	<meta property="og:site_name" content="titre du site">
-	<meta property="og:description" content="texte de description à afficher sur les
-	réseaux sociaux">
+	<meta property="og:url" content="http://www.jonathanfechner.fr/">
+	<meta property="og:image" content="http://www.jonathanfechner.fr/img/logo-big.png">
+	<meta property="og:site_name" content="Jonathan Fechner - Gestion">
+	<meta property="og:description" content="Jonathan Fechner vous conseille sur la mission de chef de projet">
 	<!-- END Open Graph-->
 	<link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="lib/font-awesome.min.css">
--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr-FR">
 <head>
-	<title>Jonathan Fechner - accueil</title>
+	<title>Jonathan Fechner - Accueil</title>
 	<!-- Dublin Core -->
 	<meta charset="utf-8">
 	<meta name="DC.title" content="Jonathan Fechner">
 	<meta name="DC.creator" content="Jonathan Fechner">
 	<meta name="DC.subject" content="referencement google, strategie de positionnement, consultant, gestion de projet, ingénierie multimédia, expertise, site internet">
-	<meta name="DC.description" content="Site de Jonathan Fechner expert en referencement et positionnement de site internet">
+	<meta name="DC.description" content="Site de Jonathan Fechner expert en referencement, positionnement de site internet et gestion de projet">
 	<meta name="DC.publisher" content="Damien Duvernois,Gauthier Flichy, Paul Dabin">
 	<meta name="DC.contributor" content="Shana Enkaoua, Rami El Khodry, Niki Gharabaghi">
 	<meta name="DC.date" content="17/11/2015">
 	<meta name="DC.type" content="SEO">
 	<meta name="DC.format" content="website">
-	<meta name="DC.identifier" content="url du site">
+	<meta name="DC.identifier" content="http://www.jonathanfechner.fr/">
 	<meta name="DC.language" content="fr-FR">
 	<meta name="DC.coverage" content="World">
 	<meta name="DC.rights" content="&copy; Jonathan Fechner">
@@ -23,16 +23,15 @@
 	<meta name="keywords" content="referencement google, strategie de positionnement, consultant, gestion de projet, ingénierie multimédia, expertise, site internet">
 	<meta name="author" content="Jonathan Fechner">
 	<meta name="robots" content="index">
-	<meta name="Indentifier-URL" content="url du site">
+	<meta name="Indentifier-URL" content="http://www.jonathanfechner.fr/">
 	<!-- END Référencement -->
 	<!-- Open Graph -->
 	<meta property="og:title" content="Jonathan Fechner">
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="url du site">
-	<meta property="og:image" content="url de l'image à afficher">
-	<meta property="og:site_name" content="titre du site  Jonathan Fechner">
-	<meta property="og:description" content="texte de description à afficher sur les
-	réseaux sociaux">
+	<meta property="og:url" content="http://www.jonathanfechner.fr/">
+	<meta property="og:image" content="http://www.jonathanfechner.fr/img/logo-big.png">
+	<meta property="og:site_name" content="Jonathan Fechner - Accueil">
+	<meta property="og:description" content="Jonathan Fechner expert en referencement et gestion de projet">
 	<!-- END Open Graph-->
 	<link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="lib/font-awesome.min.css">
